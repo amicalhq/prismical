0468b37d2d36892bae0af2d56b44b8a2ffbbd16f
Naomi Chopra <naomi.chopra.021@gmail.com>
2025-05-10 01:22:26 +0530

    feat(www): hero and FAQ on home page (#4)
    * add hero
    
    * fix names
    
    * add faq

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -1,6 +1,11 @@
+import { Hero } from "@/components/ui/hero";
+import { GeneralFAQ } from "@/components/ui/general-faq";
+
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col">
+      <Hero />
+      <GeneralFAQ />
     </main>
   );
 }
